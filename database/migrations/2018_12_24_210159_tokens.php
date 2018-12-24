57dbf0cd3a0588cdc55bb8267a75911aafb8adc1
donaldhansen <dxhans5@gmail.com>
2018-12-24 14:04:37 -0700

    database connection

---
+++ database/migrations/2018_12_24_210159_tokens.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Tokens extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tokens', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('access_token');
+            $table->int('expires_in');
+            $table->string('refresh_token');
+            $table->string('token_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tokens');
+    }
+}
